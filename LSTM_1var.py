c84d2c71561b77b22f0bba728ff46f75e79509f0
nyuhanc <pavsek.aljaz@gmail.com>
2023-09-28 14:16:01 +0200

    commit 28.9.2023

--- LSTM_1var.py
+++ LSTM_1var.py
@@ -27,6 +27,10 @@ def normalize_data(df):
             df[col] = (df[col] - df[col].mean()) / df[col].std()
     return df
 
+# Normalized Root Mean Squared Error (with STD)
+def NRMSE(y_true, y_pred):
+    return np.sqrt(np.mean((y_true - y_pred)**2)) / np.std(y_true)
+
 # Parameters
 n_lags = 10
 targets = ['xmeas_1'] #[f'xmeas_{i}' for i in range(1, 41+1)]
@@ -50,17 +54,25 @@ df = df_train.dropna()
 # Defragment the dataframe
 df_train = df_train.copy()
 
-# Train-val-test split (80/20/20); but a multiple of max batch size (512)
-train0_size = int(0.8 * len(df))
-train0_size = train0_size - (train0_size % 512)
-# Train-val-test split (80/20/20)
-train_size = int(0.8 * train0_size)
+# Train-val-test split (80/19/1), but all dividable with 512 (chosen as max batch size)
+train_size = int(0.8 * len(df))
 train_size = train_size - (train_size % 512)
-train_df, val_df, test_df = df[:train_size], df[train_size:train0_size], df[train0_size:(len(df) // 512) * 512]
+train_df = df[:train_size]
+
+val_size = int(0.19 * len(df))
+val_size = val_size - (val_size % 512)
+val_df = df[train_size:train_size + val_size]
+
+test_size = len(df) - train_size - val_size
+test_size = test_size - (test_size % 512)
+test_df = df[train_size + val_size:train_size + val_size + test_size]
+
+print(f"Train size: {len(train_df)}")
+print(f"Val size: {len(val_df)}")
+print(f"Test size: {len(test_df)}")
 
 # Number of features
 num_features = len(xmv_variables) + 1 # 11 xmv variables + 1 target variable
-
 
 # --------- Define LSTM model constructor -------------
 def create_lstm_model(input_shape, lstm_layers=[50, 50], dropout_rate=0.1, stateful=False, batch_size=None):
@@ -97,9 +109,9 @@ for target in targets:
     # Hyperparameters for the model
     input_shape = (X_train.shape[1], X_train.shape[2])
     lstm_layers = [50, 50, 50]  # Three LSTM layers with 50 units each
-    dropout_rate = 0.1
+    dropout_rate = 0.05
     stateful = True  # stateful LSTM !!!!!
-    batch_size = 32  # Specify batch size for stateful LSTM
+    batch_size = 32  # Must satisfy 512 % batch_size == 0 (look at the train-val-test split above)
 
     # Create the LSTM model
     model = create_lstm_model(input_shape=input_shape,
@@ -115,54 +127,58 @@ for target in targets:
 
     # Fit the model
     history = model.fit(
-        x=X_train, #
+        x=X_train,
         y=y_train,
         validation_data=(X_val, y_val),
-        batch_size=32,  # Must satisfy 512 % batch_size == 0
-        epochs=5,
+        batch_size=batch_size,  # Must satisfy 512 % batch_size == 0
+        epochs=100,
         shuffle=False,  # Important! Do not shuffle the data when stateful=True
         callbacks=[
             keras.callbacks.EarlyStopping(monitor='val_loss', patience=7, mode='min')
         ]
     )
 
-    # # Score the model
-    # y_pred = model.predict(X_test)
-    # y_pred = y_pred.reshape(-1)
-    # y_test = y_test.values
-    # print(f"RMSE: {np.sqrt(mean_squared_error(y_test, y_pred))}")
-
-    # Before entering the loop for recursive forecasting, reset the LSTM internal state
-    model.reset_states()
+    # Score the model
+    print(f"NRMSE on val. set: "
+          f"{NRMSE(model.predict(X_val).reshape(-1), y_val)}")
 
     # Recursive forecasting
     predictions = []
-    xmeas_lags = y_test[:n_lags].tolist()
+    xmeas_lags = test_df[[f"{target}_lag{i}" for i in range(1, n_lags + 1)]].iloc[0].tolist()[::-1]
+    predict_n_steps = len(y_test)
 
-    predict_n_steps = 128
-
-    for i in range(predict_n_steps):  # len(y_test) - n_lags):
+    for i in range(predict_n_steps):
         # Prepare input data for prediction
         input_data = {f"{target}_lag{j + 1}": xmeas_lags[-(j + 1)] for j in range(n_lags)}
         for var in xmv_variables:
             for lag in range(0, n_lags):
-                input_data[f"{var}_lag{lag}"] = test_df.iloc[i + n_lags - lag][var]
+                input_data[f"{var}_lag{lag}"] = test_df[f"{var}_lag{lag}"].iloc[i]
         input_df = pd.DataFrame([input_data])
-        input_df = input_df.to_numpy(dtype='float32').reshape(n_lags, num_features)
+        input_tensor = input_df.to_numpy(dtype='float32').reshape(n_lags, num_features)
+
+        # Assume input_tensor is your single sample of shape (time_steps, features).
+        # Since the model expects a batch of samples (it predicts batch_size samples
+        # in parallel), we need to add a dummy batch dimension, i.e. convert the input
+        # tensor from shape (time_steps, features) to (batch_size, time_steps, features)
+        # by duplicating the sample along the batch dimension. We are duplicating the
+        # sample instead of adding zeros so that the inner states of the LSTM will
+        # remain the same as if we predicted one sample without a batch.
+        real_batch = np.repeat(input_tensor[np.newaxis, ...], repeats=batch_size, axis=0)
 
         # Make a prediction
-        prediction = model.predict(input_df[np.newaxis, ...])[0]
+        prediction = model.predict(real_batch)[0]  # [0] to get scalar instead of array of size 1
         predictions.append(prediction)
 
         # Update xmeas_1 lags with the new prediction
         xmeas_lags.append(prediction)
 
-    # Calculate RMSE
-    rmse_value = mean_squared_error(y_test[n_lags:n_lags + predict_n_steps], predictions, squared=False)
+    # Calculate NRMSE  ( [:predict_n_steps] because if predict_n_steps < len(y_test) )
+    nrmse_value = NRMSE(y_test[:predict_n_steps], predictions)
+    rmse_value = mean_squared_error(y_test[:predict_n_steps], predictions, squared=False)
 
     # Plot predictions vs. actuals
-    plot_samples = predict_n_steps
-    actual_values = y_test[n_lags:n_lags + plot_samples]
+    plot_samples = 150
+    actual_values = y_test[:plot_samples]
     predicted_values = predictions[:plot_samples]
 
     # Create a figure and axis object
@@ -176,7 +192,7 @@ for target in targets:
     # Add grid, legend, and RMSE text
     ax.grid(True)
     ax.legend(loc=1)
-    ax.text(0.985, 0.02, f'RMSE: {rmse_value:.4f}', transform=ax.transAxes, ha='right', va='bottom', fontsize=10,
+    ax.text(0.985, 0.02, f'NRMSE: {rmse_value:.4f}', transform=ax.transAxes, ha='right', va='bottom', fontsize=10,
             color='black', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.3'))
 
     # Save the plot
